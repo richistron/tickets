dcb5c1b7a409a54c327d130b72b7887f8df66e2d
Ricardo Rivas G <richistron@gmail.com>
2016-10-21 22:16:50 -0500

    purchase empty model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161022030126) do
+ActiveRecord::Schema.define(version: 20161022031618) do
 
   create_table "commission_defaults", force: :cascade do |t|
     t.datetime "created_at",        null: false
@@ -64,6 +64,11 @@ ActiveRecord::Schema.define(version: 20161022030126) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "name"
+  end
+
+  create_table "purchases", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "tickets", force: :cascade do |t|
